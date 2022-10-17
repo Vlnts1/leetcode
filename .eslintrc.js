15ba9db5f41bde4c8d616b51ceea534c205f66c0
Vlnts <valentsionovaviktoria@gmail.com>
2022-10-18 01:51:17 +0300

    Add eslint, prettier

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'prettier'],
+  root: true,
+
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'error',
+    '@typescript-eslint/no-inferrable-types': 'off',
+    '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
+    '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/padding-line-between-statements': [
+      'error',
+      {
+        blankLine: 'always',
+        prev: '*',
+        next: ['interface', 'type'],
+      },
+    ],
+  },
+};
